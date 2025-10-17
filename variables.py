bfecd05ffaf068b38a703d1cec608e7fc0fe1014
Xafizov G'iyosiddin <153916780+xafizovdev@users.noreply.github.com>
2025-10-17 23:54:56 +0500

    Add variables and print statements in variables.py

---
+++ variables.py
@@ -0,0 +1,21 @@
+# 1
+hello = "Hello World"
+print(hello)
+
+# 2
+message = "Hello. My name is G'iyosiddin"
+print(message)
+
+# 2.1
+message = "Today is the fifth day of the year two thousand twenty-one."
+print(message)
+
+# 3
+# class = "This variable is invalid, because 'class' is a reserved keyword."
+# print(class)
+
+# 4
+radius = 5
+pi = 3.14159
+circle_area = pi * radius**2
+print("The area of a circle with radius", radius, "is", circle_area)
